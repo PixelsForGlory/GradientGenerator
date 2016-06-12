99829d0607ec5df199dab33be5799411fd173fb5
afuzzyllama <karcieri@gmail.com>
2016-06-12 18:46:39 -0400

    Improving build process

--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
 # Run unit test
 Copy-Item $env:APPVEYOR_BUILD_FOLDER\GradientGenerator\bin\Release\GradientGenerator.* -Destination $env:APPVEYOR_BUILD_FOLDER\GradientGeneratorTest\bin\Release\ -Force
 vstest.console $env:APPVEYOR_BUILD_FOLDER\GradientGeneratorTest\bin\Release\GradientGeneratorTest.dll /Settings:$env:APPVEYOR_BUILD_FOLDER\GradientGeneratorTest\test.runsettings /logger:Appveyor
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+
